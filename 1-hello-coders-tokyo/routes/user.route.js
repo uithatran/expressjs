9eaa4ba25427b62d23cc7f568df1fbf6f6780d0c
Ha Tran Thai <ttha@tma.com.vn>
2020-03-04 16:38:23 +0700

    add Middleware into postCreate

--- 1-hello-coders-tokyo/routes/user.route.js
+++ 1-hello-coders-tokyo/routes/user.route.js
@@ -1,6 +1,8 @@
 var express = require('express');
 
 var controller = require('../controllers/user.controller');
+var validate = require('../validate/user.validate');
+
 // BT: CRUD = Create Retrieve Update Delete
 var router = express.Router();
 
@@ -9,6 +11,6 @@ router.get('/search', controller.search);
 router.get('/create', controller.create);
 router.get('/:id', controller.userId);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 
 module.exports = router;
