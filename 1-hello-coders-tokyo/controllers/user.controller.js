4f291c1ab93034c672782c1f98c88f580d440317
Ha Tran Thai <ttha@tma.com.vn>
2020-03-04 17:02:34 +0700

    add Middleware for postCreate into validate folder

--- 1-hello-coders-tokyo/controllers/user.controller.js
+++ 1-hello-coders-tokyo/controllers/user.controller.js
@@ -28,7 +28,6 @@ module.exports.create = function (req, res) {
 
 module.exports.userId = function (req, res) {
   var id = req.params.id;
-  console.log(typeof id);
   var user = db.get('users').find({ id: id }).value();
   res.render('users/view', {
     user: user
@@ -37,24 +36,6 @@ module.exports.userId = function (req, res) {
 
 module.exports.postCreate = function (req, res) {
   req.body.id = shortid.generate();
-  var errors = [];
-
-  if(!req.body.name) {
-    errors.push('Name is required');
-  }
-
-  if(!req.body.phone) {
-    errors.push('Phone is required');
-  }
-
-  //Learn falsy and trutly
-  if(errors.length) {
-    res.render('users/create', {
-      errors: errors,
-      values: req.body
-    });
-    return;
-  }
   db.get('users').push(req.body).write()
   res.redirect('/users')
 }
